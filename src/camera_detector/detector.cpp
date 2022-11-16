c59076254ea54abc6a547daee1296d45eba52975
Stephen Ninan <nstephen@tamu.edu>
2022-11-15 18:31:11 -0600

    Update Cmake

--- src/camera_detector/detector.cpp
+++ src/camera_detector/detector.cpp
@@ -7,6 +7,11 @@ Detector::Detector(std::string model_path,std::string topic,ros::NodeHandle& n)
     nh = n;
     img_pub = nh.advertise<sensor_msgs::Image>("/img_with_bbox",100);
     img_data_pub = nh.advertise<vision_msgs::Detection2DArray>("/img_detections",100);
-    img_sub = nh.subscribe<sensor_msgs::Image>(topic,100,callback);
+    img_sub = nh.subscribe<sensor_msgs::Image>(topic,100,&Detector::callback,this);
     ROS_INFO("Subscribers and Publishers successfully loaded");
+}
+
+void Detector::callback(const sensor_msgs::ImageConstPtr& img)
+{
+    
 }
