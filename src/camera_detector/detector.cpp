09ad08b7a12a232df17f848efd8e60714bb437d9
Stephen Ninan <nstephen@tamu.edu>
2022-11-15 18:45:22 -0600

    Working Model load

--- src/camera_detector/detector.cpp
+++ src/camera_detector/detector.cpp
@@ -3,7 +3,7 @@
 Detector::Detector(std::string model_path,std::string topic,ros::NodeHandle& n)
 {
     detector.reset(new cppflow::model(model_path));
-    ROS_INFO("Detecotr model successfully loaded");
+    ROS_INFO("Detector model successfully loaded");
     nh = n;
     img_pub = nh.advertise<sensor_msgs::Image>("/img_with_bbox",100);
     img_data_pub = nh.advertise<vision_msgs::Detection2DArray>("/img_detections",100);
@@ -13,5 +13,5 @@ Detector::Detector(std::string model_path,std::string topic,ros::NodeHandle& n)
 
 void Detector::callback(const sensor_msgs::ImageConstPtr& img)
 {
-    
+
 }
--- src/camera_detector/ssd_detector.cpp
+++ src/camera_detector/ssd_detector.cpp
@@ -6,8 +6,9 @@ int main(int argc,char** argv)
     ros::init(argc,argv,"Detector");
     ros::NodeHandle nh;
 
-    std::string model_full_path = "";
-    std::string img_topic = "";
+    std::string model_full_path , img_topic;
+    nh.getParam("/ssd_detector/img_topic",img_topic);
+    nh.getParam("ssd_detector/ssd_model_path",model_full_path);
 
     std::shared_ptr<Detector> ssd(new Detector(model_full_path,img_topic,nh));
 
